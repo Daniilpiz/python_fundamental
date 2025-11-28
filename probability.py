2023c823eecc3c8a35030117a02e47a55e76441c
Даниил <daniildaniil2107@gmail.com>
2025-11-28 18:18:47 +0300

    modified:   probability.py  в ушах также доминик джокер

--- probability.py
+++ probability.py
@@ -19,7 +19,7 @@ SQRT_TWO_PI = math.sqrt(2*math.pi)
 def normal_pdf(x:float, mu:float = 0, sigma:float = 1) -> float:
     return (math.exp(-(x-mu)**2 / 2 /sigma**2) / (SQRT_TWO_PI*sigma))
 
-xs = [x/10.0 for x in range(-50, 50)]
+xs = [x/10.0 for x in range(-100, 100)]
 plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs],'-', label = 'мю = 0, сигма = 1', color = "green")
 plt.plot(xs, [normal_pdf(x, sigma = 2) for x in xs],'.', label = 'мю = 0, cигма = 2',color = 'red')
 plt.plot(xs, [normal_pdf(x, sigma = 0.5) for x in xs],'--', label = 'мю = 0, cигма = 0.5',color = 'blue')
@@ -46,6 +46,7 @@ plt.show()
 
 #инвертирование кумулятивной функции
 #используем бинарный поиск для нахождения приближенного значения
+
 def inverse_normal_cdf(p:float, mu:float = 0, sigma:float = 1, tolerance: float = 0.00001)-> float:
     if mu != 0 or sigma != 1:
         return mu+sigma+inverse_normal_cdf(p, tolerance=tolerance)
@@ -55,10 +56,20 @@ def inverse_normal_cdf(p:float, mu:float = 0, sigma:float = 1, tolerance: float 
 
     while hi_z-low_z>tolerance:
         mid_z = (low_z+hi_z) / 2 #рассмотреть среднюю точку
-        mid_p = normal_cdf(mid_z) # и рассмотреть значение cdf от этой точки
+        mid_p = normal_cdf(mid_z, mu=0, sigma=1) # и рассмотреть значение cdf от этой точки
 
         if mid_p<p:
             low_z = mid_z #средняя точка низкая искать выше
 
         else: hi_z = mid_z #средняя точка высокая искать ниже
     return mid_z
+
+
+plt.plot(xs, [inverse_normal_cdf(x, sigma=1, mu= 0) for x in xs],'-', label = 'мю = 0, сигма = 1', color = "green")
+plt.plot(xs, [inverse_normal_cdf(x, sigma = 2, mu= 0) for x in xs],'.', label = 'мю = 0, cигма = 2',color = 'red')
+plt.plot(xs, [inverse_normal_cdf(x, sigma = 0.5, mu = 0) for x in xs],'--', label = 'мю = 0, cигма = 0.5',color = 'blue')
+plt.plot(xs, [inverse_normal_cdf(x, sigma = 1, mu= -1) for x in xs],':', label = 'мю = -1, cигма = 1',color = 'black')
+
+plt.legend(loc = 4)
+plt.title("Различные инвертированные нормальные кумулятивные функции")
+plt.show()
